5c9c9728ad0cc390e942c1a8feef62a10465fe84
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-22 10:15:33 +0200

    Ajout de la configuration de connection à la bdd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import fr.fms.shop.config.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection connection = DatabaseConnection.getConnection()) {
+
+            System.out.println("Connexion à MariaDB réussie !");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
     }
 }
